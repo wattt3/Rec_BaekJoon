62e0d81b3fe46c01be4215bd2544aabe6e69baa8
jaeseung-lee <kevin0969@naver.com>
2022-04-05 22:38:08 +0900

    [Front] problem api 요청하는 것 구현 1. problem api 요청하는 것 구현 2. problempage 와 Problem-recommend 합침

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,15 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./routes/Home";
-import NewHome from "./routes/NewHome";
 import ProblemRecommend from "./routes/ProblemRecommend";
-import ProblemPage from "./routes/ProblemPage";
 
 export enum routes {
   HOME = "/",
   PROBLEM_RECOMMEND = "/problem-recommend",
-  PROBLEM = "/problem",
-  NEW_HOME = "/new-home",
 }
 
 function App() {
@@ -18,12 +14,10 @@ function App() {
       <Header />
       <Routes>
         <Route path={routes.HOME} element={<Home />}></Route>
-        <Route path={routes.NEW_HOME} element={<NewHome />}></Route>
         <Route
           path={routes.PROBLEM_RECOMMEND}
           element={<ProblemRecommend />}
         ></Route>
-        <Route path={routes.PROBLEM} element={<ProblemPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
