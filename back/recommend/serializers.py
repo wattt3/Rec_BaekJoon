7de515a98cd08e2a38f40e859e35a740760ffc5f
kimsungjoon <retioonz@gmail.com>
2022-04-13 12:05:16 +0900

    추가 구현

--- back/recommend/serializers.py
+++ back/recommend/serializers.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
 from .models import User, Problem, Recommend
 
+import urllib.request
+import urllib.parse
+import json
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         fields = (
@@ -10,12 +14,23 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
 
 class ProblemSerializer(serializers.ModelSerializer):
+    problemMetadata = serializers.SerializerMethodField()
+
     class Meta:
         fields = (
             'id',
             'number',
+            'problemMetadata',
         )
         model = Problem
+
+    def get_problemMetadata(self, obj):
+        url = urllib.request.Request("https://solved.ac/api/v3/problem/show?problemId=" + (str)(obj.number))
+        url.add_header("Content-Type", "application/json")
+        data = urllib.request.urlopen(url)
+        data = data.read()
+        data = json.loads(data.decode('utf-8'))
+        return data["titleKo"]
 
 class RecommendSerializer(serializers.ModelSerializer):
     class Meta:
--- back/recommend/views.py
+++ back/recommend/views.py
@@ -1,25 +1,89 @@
 from django.shortcuts import render
+from numpy import number
 from rest_framework import generics
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import User, Problem, Recommend
 from .serializers import UserSerializer, ProblemSerializer, RecommendSerializer
 
-class ListUser(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+@api_view(['GET', 'POST'])
+def user_list(request):
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
 
-class DetailUser(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    elif request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class ListProblem(generics.ListCreateAPIView):
-    queryset = Problem.objects.all()
-    serializer_class = ProblemSerializer
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-class DetailProblem(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Problem.objects.all()
-    serializer_class = ProblemSerializer
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+@api_view(['GET', 'POST'])
+def problem_list(request):
+    if request.method == 'GET':
+        problems = Problem.objects.all()
+        serializer = ProblemSerializer(problems, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ProblemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def problem_detail(request, pk):
+    try:
+        problem = Problem.objects.get(pk=pk)
+    except Problem.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ProblemSerializer(problem)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ProblemSerializer(problem, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        problem.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class ListRecommend(generics.ListCreateAPIView):
