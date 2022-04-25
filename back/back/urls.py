18dff8c776c54ef0656bd860529bec92ed9a04ab
kimsungjoon <retioonz@gmail.com>
2022-04-26 01:28:52 +0900

    Update urls.py

--- back/back/urls.py
+++ back/back/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('recommend.urls')),
+    path('joljack-front/api/', include('recommend.urls')),
 ]
