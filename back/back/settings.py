2bb0bd9b7106764d67cab281a2bb65fef485563d
wattt3 <chleodnr3@gmail.com>
2022-04-28 00:38:08 +0900

    docker-setting-complete

--- back/back/settings.py
+++ back/back/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 import os
 
@@ -138,6 +139,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = os.path.join(BASE_DIR,"..","Front","build","static","")
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
