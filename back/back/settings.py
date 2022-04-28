e898b845eb3a770bf55179c58406c687ccf7cbf7
Yeongho Choi <chldudgh8256@naver.com>
2022-04-28 19:35:24 +0900

    장고 secret key 수정

--- back/back/settings.py
+++ back/back/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!r4e85+!&tln(e@^bqv7_*3b*&o!it)5ts!u4t=f7k6_^%lu%!'
+# SECRET_KEY = 'django-insecure-!r4e85+!&tln(e@^bqv7_*3b*&o!it)5ts!u4t=f7k6_^%lu%!'
+SECRET_KEY = 'django-insecure-tln(e@^bqv7_*3b*&o!it)5ts!u4t=f7k6_^%lu%!'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
